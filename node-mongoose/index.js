db991aa68cf3594836b16a969609908a4ffcbb75
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2020-12-19 20:48:22 +0530

    Node Mongoose part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,26 +8,19 @@ const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log('COnnected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name:'Uthappizza',
         description: 'test'
-    });
+    })
+    .then((dishes)=>{
+        console.log(dishes);
 
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
